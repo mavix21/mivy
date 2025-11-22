2eeb9f07a01a915ac254c5a7193c2776ac24adea
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-22 11:50:59 -0300

    feat: dashboard route example

---
+++ src/app/(auth)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+      <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
+      <p className="text-lg text-gray-600">
+        Welcome to your dashboard! Here you can manage your account and view
+        your data.
+      </p>
+    </div>
+  );
+}

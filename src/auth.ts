75df543a7e4ceb71ecf9b64da1ed2b6ec44e3191
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-22 11:46:32 -0300

    feat: add Google OAuth support and update plugin order in auth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -3,10 +3,18 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 import { jwt } from "better-auth/plugins";
 import { jwtClient } from "better-auth/client/plugins";
+import { env } from "./lib/env";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
-  plugins: [jwt(), jwtClient()],
+  plugins: [nextCookies(), jwt(), jwtClient()],
 });

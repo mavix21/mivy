9714c9315ba447674331f1bb0d2c02aa2335eab0
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-22 11:52:23 -0300

    refactor: remove unused currentUser import and adjust container height

--- src/app/(unauth)/sign-in/page.tsx
+++ src/app/(unauth)/sign-in/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { useCurrentUser, useIsSignedIn } from "@coinbase/cdp-hooks";
+import { useIsSignedIn } from "@coinbase/cdp-hooks";
 import SignInScreen from "./_components/sign-in";
 
 export default function AuthComponent() {
   const { isSignedIn } = useIsSignedIn();
-  const { currentUser } = useCurrentUser();
 
   return (
-    <div className="flex h-full w-full items-center justify-center p-4">
+    <div className="flex h-svh w-full items-center justify-center p-4">
       {isSignedIn ? <div>Welcome! You're signed in.</div> : <SignInScreen />}
     </div>
   );
